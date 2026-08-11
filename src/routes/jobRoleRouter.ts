e6c480794e51176db69062dbb938cdc7258d5870
oskar-skomra <oskar.skomra@kainos.com>
2026-08-11 09:59:41 +0200

    injected JobRoleDao through DI to JobRoleService

--- src/routes/jobRoleRouter.ts
+++ src/routes/jobRoleRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { JobRoleController } from "../controllers/jobRoleController";
 import { JobRoleService } from "../services/jobRoleService";
+import { JobRoleDaoImpl } from "../dao/jobRoleDaoImpl";
 
 const router = Router();
-const controller = new JobRoleController(new JobRoleService());
+const controller = new JobRoleController(new JobRoleService(new JobRoleDaoImpl()));
 
 router.get("/", async (_req, res) => controller.getAll(_req, res));
 
--- src/services/jobRoleService.ts
+++ src/services/jobRoleService.ts
@@ -1,10 +1,9 @@
 import type { JobRoleDao } from "../dao/jobRoleDao";
-import { JobRoleDaoImpl } from "../dao/jobRoleDaoImpl";
 import { mapJobRoleToJobRoleResponses } from "../mappers/jobRoleMapper";
 import type { JobRoleResponse } from "../models/JobRoleResponse";
 
 export class JobRoleService {
-	constructor(private dao: JobRoleDao = new JobRoleDaoImpl()) {}
+	constructor(private dao: JobRoleDao) {}
 
 	async getAll(): Promise<JobRoleResponse[]> {
 		const jobRoles = await this.dao.getAll();
