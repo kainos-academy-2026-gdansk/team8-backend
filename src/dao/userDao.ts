9bceea0729c98f09063108b80743c3e2de34f9c1
oskar-skomra <oskar.skomra@kainos.com>
2026-08-13 09:48:54 +0200

    changed prisma to return only minimal data

--- src/dao/userDao.ts
+++ src/dao/userDao.ts
@@ -2,5 +2,5 @@ import type { RegisterResponseDto } from "../dtos/UserDto";
 
 export interface UserDao {
 	emailExists(email: string): Promise<boolean>;
-	register(email: string, password: string): Promise<RegisterResponseDto>;
+	register(email: string, passwordHash: string): Promise<RegisterResponseDto>;
 }
--- src/dao/userDaoImpl.ts
+++ src/dao/userDaoImpl.ts
@@ -5,17 +5,24 @@ import type { UserDao } from "./userDao";
 
 export class UserDaoImpl implements UserDao {
 	async emailExists(email: string): Promise<boolean> {
-		return !!(await prisma.user.findUnique({ where: { email } }));
+		return !!(await prisma.user.findUnique({
+			where: { email },
+			select: { id: true },
+		}));
 	}
 
 	async register(
 		email: string,
-		password: string,
+		passwordHash: string,
 	): Promise<RegisterResponseDto> {
 		const user = await prisma.user.create({
 			data: {
 				email,
-				passwordHash: password,
+				passwordHash,
+			},
+			select: {
+				email: true,
+				role: true,
 			},
 		});
 		return { email: user.email, role: fromPrismaUserRole(user.role) };
